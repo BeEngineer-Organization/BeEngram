59d34df07b8636543f3601d65635d6ccf8444214
Luc Khai Hai <lkh42t@gmail.com>
2021-11-08 23:15:57 +0900

    style for auth forms

--- main/forms.py
+++ main/forms.py
@@ -5,11 +5,9 @@ from .models import Comment, Post, User
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text="Required")
-
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email")
 
 
 class PostForm(forms.ModelForm):
